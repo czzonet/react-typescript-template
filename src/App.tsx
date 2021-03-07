ec049e77d001c8ebecc250a479ce38147ead7849
czzonet <czzonet@163.com>
2021-03-07 08:32:10 +0800

    webpack init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>React</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+        reiciendis voluptate, perferendis maiores possimus voluptatem officia
+        harum, voluptas accusamus nobis commodi aliquid doloremque eaque
+        eligendi, dolores corporis ipsam quod non.
+      </p>
+      <input type="text" />
+    </div>
+  );
+}
