2b13fc12e5c67799f1f4dfc3e5416058d05d7f82
czzonet <czzonet@163.com>
2021-05-01 16:20:44 +0800

    feat: add app 添加app主入口

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import styles from "./AppStyle.scss";
+
+import { Button } from "antd";
+import { Template } from "./template";
 
 export default function App() {
   return (
-    <div>
-      <h1>React</h1>
+    <div className={styles["app"]}>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
         reiciendis voluptate, perferendis maiores possimus voluptatem officia
         harum, voluptas accusamus nobis commodi aliquid doloremque eaque
         eligendi, dolores corporis ipsam quod non.
       </p>
-      <input type="text" />
+      <Template></Template>
     </div>
   );
 }
